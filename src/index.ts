6dc51e79b58bcfb46790d3abeebc147410e6c1ff
Akash Raikwar <akashraikwar.mongoosetech@gmail.com>
2026-03-14 01:06:30 +0530

    fix issue

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-employee-id']
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Request logging middleware
 app.use((req, res, next) => {
